49ceb0c4e52bd7c30307a32146e949bcda7b1a19
ChivenZhang <1759774248@qq.com>
2024-11-18 10:19:29 +0800

    update

--- OpenMS/Service/Private/Service.cpp
+++ OpenMS/Service/Private/Service.cpp
@@ -30,7 +30,7 @@ void Service::startup(int argc, char** argv)
 	if (IService::Instance()) return;
 	m_Instance = this;
 
-	auto config = AUTOWIRE2_THIS(IValue, "registry.server")->value<ServerConfig>();
+	auto config = AUTOWIRE2_DATA(IValue, "registry.server")->value<ServerConfig>();
 	m_Reactor = TNew<TCPServerReactor>(
 		IPv4Address::New(config.ip, config.port), config.backlog, config.workers,
 		TCPServerReactor::callback_t{
@@ -54,7 +54,7 @@ void Service::shutdown()
 
 TString Service::property(TStringView name) const
 {
-	auto source = AUTOWIRE(IProperty)::bean();
+	auto source = AUTOWIRE_DATA(IProperty);
 	if (source == nullptr) return TString();
 	return source->property(name);
 }
