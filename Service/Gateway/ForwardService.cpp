32c32eef631438951de34b0765f806dbf78c8da2
ChivenZhang <chivenzhang@gmail.com>
2025-12-01 16:03:09 +0800

    update

--- Service/Gateway/ForwardService.cpp
+++ Service/Gateway/ForwardService.cpp
@@ -20,69 +20,48 @@ ForwardService::ForwardService(MSHnd<IChannel> client)
 
 IMailTask ForwardService::read(IMail mail)
 {
-	if (mail.Type == 0)
+	if (mail.Type & OPENMS_MAIL_TYPE_CLIENT)
 	{
-		if (sizeof(request_t) <= mail.Body.size())
+		if (mail.Type & OPENMS_MAIL_TYPE_REQUEST)
 		{
-			auto& request = *(request_t*)mail.Body.data();
-			method_t method;
+			send(mail);
+		}
+		else if (mail.Type & OPENMS_MAIL_TYPE_RESPONSE)
+		{
+			if (auto client = m_ClientChannel.lock())
 			{
-				MSMutexLock lock(m_MutexMethod);
-				auto result = m_MethodMap.find(request.Method);
-				if (result != m_MethodMap.end()) method = result->second;
-			}
-			if (method)
-			{
-				auto input = MSStringView(request.Buffer, mail.Body.size() - sizeof(request_t));
-				co_await method(input);
+				auto userID = (uint32_t)client->getContext()->userdata();
+				MSString buffer(sizeof(MailView) + mail.Body.size(), 0);
+				auto& mailView = *(MailView*)buffer.data();
+				mailView.From = name();
+				mailView.To = MSHash("client:" + std::to_string(userID));
+				mailView.Date = mail.Date;
+				mailView.Type = mail.Type;
+				if (mail.Body.empty() == false) ::memcpy(mailView.Body, mail.Body.data(), mail.Body.size());
+				client->writeChannel(IChannelEvent::New(buffer));
 			}
 		}
 	}
 	else
 	{
-		if (mail.Type & OPENMS_MAIL_TYPE_CLIENT)
+		if (mail.Type & OPENMS_MAIL_TYPE_REQUEST)
 		{
-			if (mail.Type & OPENMS_MAIL_TYPE_REQUEST)
+			if (auto client = m_ClientChannel.lock())
 			{
-				send(mail);
-			}
-			else if (mail.Type & OPENMS_MAIL_TYPE_RESPONSE)
-			{
-				if (auto client = m_ClientChannel.lock())
-				{
-					auto userID = (uint32_t)client->getContext()->userdata();
-					MSString buffer(sizeof(MailView) + mail.Body.size(), 0);
-					auto& mailView = *(MailView*)buffer.data();
-					mailView.From = name();
-					mailView.To = MSHash("client:" + std::to_string(userID));
-					mailView.Date = mail.Date;
-					mailView.Type = mail.Type;
-					if (mail.Body.empty() == false) ::memcpy(mailView.Body, mail.Body.data(), mail.Body.size());
-					client->writeChannel(IChannelEvent::New(buffer));
-				}
+				auto userID = (uint32_t)client->getContext()->userdata();
+				MSString buffer(sizeof(MailView) + mail.Body.size(), 0);
+				auto& mailView = *(MailView*)buffer.data();
+				mailView.From = name();
+				mailView.To = MSHash("client:" + std::to_string(userID));
+				mailView.Date = mail.Date;
+				mailView.Type = mail.Type;
+				if (mail.Body.empty() == false) ::memcpy(mailView.Body, mail.Body.data(), mail.Body.size());
+				client->writeChannel(IChannelEvent::New(buffer));
 			}
 		}
-		else
+		else if (mail.Type & OPENMS_MAIL_TYPE_RESPONSE)
 		{
-			if (mail.Type & OPENMS_MAIL_TYPE_REQUEST)
-			{
-				if (auto client = m_ClientChannel.lock())
-				{
-					auto userID = (uint32_t)client->getContext()->userdata();
-					MSString buffer(sizeof(MailView) + mail.Body.size(), 0);
-					auto& mailView = *(MailView*)buffer.data();
-					mailView.From = name();
-					mailView.To = MSHash("client:" + std::to_string(userID));
-					mailView.Date = mail.Date;
-					mailView.Type = mail.Type;
-					if (mail.Body.empty() == false) ::memcpy(mailView.Body, mail.Body.data(), mail.Body.size());
-					client->writeChannel(IChannelEvent::New(buffer));
-				}
-			}
-			else if (mail.Type & OPENMS_MAIL_TYPE_RESPONSE)
-			{
-				send(mail);
-			}
+			send(mail);
 		}
 	}
 	co_return;
